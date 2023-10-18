6395fe5f957be101317a27b92ccb2c4a61581d6f
mahdi <mahdi@nixa.ca>
2023-10-18 17:09:08 -0400

    replace dummy data with real data comming from firebase

--- src/components/availblemeals-component/AvailbleMeals.jsx
+++ src/components/availblemeals-component/AvailbleMeals.jsx
@@ -1,55 +1,88 @@
+import React from "react";
+import classes from "./AvailebeleMeals-styles.module.scss";
+import Card from "../Card-component/Card";
+import MealItem from "../Meal-item-component/MealItem";
 
-import React from 'react'
-import classes from './AvailebeleMeals-styles.module.scss'
-import Card from '../Card-component/Card';
-import MealItem from '../Meal-item-component/MealItem';
+import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
 
-const AvailbleMeals = ()=> {
-    const mealsList = DUMMY_MEALS.map(meale=> 
+
+const AvailbleMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  const fetchMeals = async () => {
+    
+    const response = await fetch(
+      "https://react-http-ba1fc-default-rtdb.firebaseio.com//meals.json"
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+    const responseData = await response.json();
+    console.log(responseData);
+
+    const loadedMeals = [];
+    for (const key in responseData) {
+      loadedMeals.push({
+        id: key,
+        name: responseData[key].name,
+        description: responseData[key].description,
+        price: responseData[key].price,
+      });
+    }
+
+    setMeals(loadedMeals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    console.log("AvailbleMeals component is rendered");
+
+
+      fetchMeals().then().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+    
+    
+  }, []);
+
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if(httpError){
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meale) => (
     <MealItem
-      key={meale.id} 
+      key={meale.id}
       id={meale.id}
-      name={meale.name} 
-      description={meale.description} 
-      price={meale.price}  />);
+      name={meale.name}
+      description={meale.description}
+      price={meale.price}
+    />
+  ));
   return (
-    <section className={ classes.meals }>
+    <section className={classes.meals}>
       <Card>
-        <ul>
-            {mealsList}
-        </ul>
-
+        <ul>{mealsList}</ul>
       </Card>
-
     </section>
-  )
-}
+  );
+};
 
-export default AvailbleMeals
+export default AvailbleMeals;
